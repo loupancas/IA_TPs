45bf8cff1722f153cfd67239a0d3b043cb0fb420
loupancas <129621626+loupancas@users.noreply.github.com>
2023-10-03 23:19:26 -0500

    pequeñoscambios

---
+++ Assets/Scripts/BoidBunny.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidBunny : AllBehaviors
+{
+    //private Renderer renderer;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+        
+
+        //UpdateBoundPosition();
+        //Flocking();
+        //AddForce(Evade);
+        //AddForce(Arrive);
+
+    }
+
+    private void FlockingBehavior()
+    {
+
+
+    }
+
+
+
+
+}
