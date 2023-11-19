3e8ec81d61e8e16ce0200baf909244f4f3d51a92
loupancas <129621626+loupancas@users.noreply.github.com>
2023-11-18 23:19:34 -0500

    mayanodo

--- Assets/Scripts/TP2/Nodo.cs
+++ Assets/Scripts/TP2/Nodo.cs
@@ -7,7 +7,17 @@ public class Nodo : MonoBehaviour
     public List<Nodo> _neighbors = new List<Nodo>(); 
     int _cost = 1;
     public int Cost { get { return _cost; } }
-    
+    public LayerMask obstacleMask;
+    private void OnDrawGizmos()
+    {
+        foreach (var element in _neighbors)
+        {
+            Vector3 dir = element.transform.position - transform.position;
+            Gizmos.color = Color.white;
 
+            Gizmos.DrawLine(transform.position, element.transform.position);
 
+        }
+
+    }
 }
