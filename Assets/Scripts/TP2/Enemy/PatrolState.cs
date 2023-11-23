c79203d38897e2c08b6b2a6479d1da5b59d5032b
loupancas <129621626+loupancas@users.noreply.github.com>
2023-11-22 22:15:15 -0500

    PatrolenProceso

---
+++ Assets/Scripts/TP2/Enemy/PatrolState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState :States
+{
+    Enemy enemy;
+    public override void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
